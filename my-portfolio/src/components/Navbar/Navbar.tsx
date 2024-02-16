07f175658a6be38f971db263f5c7490e9d4b3e92
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-02-16 18:17:00 +0100

    .

--- my-portfolio/src/components/Navbar/Navbar.tsx
+++ my-portfolio/src/components/Navbar/Navbar.tsx
@@ -7,11 +7,11 @@ const navbar: React.FC = () => {
       <div className='wrapper'>
         <span>Tarcísio Menezes</span>
         <div className='social'>
-            <a href=''><img src='/github.png' alt='github'/></a>
-            <a href=''><img src='/linkedin.png' alt='linkedln'/></a>
-            <a href=''><img src='/instagram.png' alt='instagram'/></a>
-            <a href=''><img src='/facebook1.png' alt='facebook'/></a>
-            <a href=''><img src='/outlook.png' alt='e-mail'/></a>
+            <a href='https://github.com/tarcisiomenezes2023' target='blank'><img src='/github.png' alt='github'/></a>
+            <a href='https://www.linkedin.com/in/tarc%C3%ADsio-menezes-03b8a0233/' target='blank'><img src='/linkedin.png' alt='linkedln'/></a>
+            <a href='https://www.instagram.com/tarcisio.menezes19/' target='blank'><img src='/instagram.png' alt='instagram'/></a>
+            <a href='https://www.facebook.com/profile.php?id=100092629518329' target='blank'><img src='/facebook1.png' alt='facebook'/></a>
+            <a href='mailto:Tarcisiomenezes2019@hotmail.com'><img src='/outlook.png' alt='e-mail'/></a>
         </div>
       </div>
     </div>

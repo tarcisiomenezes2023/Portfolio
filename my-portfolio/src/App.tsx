4a031e75ca3813c3c664c723b568385c836f1b02
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-02-16 16:54:03 +0100

    Everything installed, created components, styles and sections

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "./app.scss"
+
+const App: React.FC = () => {
+return (
+   <div>
+    <section><a href="">Hero</a></section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+   </div>
+  )
+}
+
+export default App

6938e1501b5fb98a574ddbeba0684267cfeba5fe
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-02-17 08:26:27 +0100

    framer motion inicialization

---
+++ my-portfolio/src/components/Test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+import "./test.scss"
+const Test: React.FC = () => {
+
+  return (
+    <div className='course'>
+    <div className='box'></div>
+    </div>
+  )
+}
+
+export default Test

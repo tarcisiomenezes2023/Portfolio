42c195054f1d47dbc68dd16991e0388c842c47c9
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-02-25 01:48:06 +0100

    Side bar menu Links navigation ready.

--- my-portfolio/src/components/sidebar/Sidebar.tsx
+++ my-portfolio/src/components/sidebar/Sidebar.tsx
@@ -4,28 +4,30 @@ import './sidebar.scss';
 import ToggleButton from "./toggleButton/ToggleButton";
 import Links from './links/Links';
 
+const variants = {
+  open: {
+    clipPath: 'circle(1200px at 50px 50px)',
+    transition: {
+      type: 'spring',
+      stiffness: 20,
+    },
+  },
+
+  closed: {
+    clipPath: 'circle(30px at 50px 50px)',
+    transition: {
+      delay: 0.5,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+
 const Sidebar: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
 
-  const variants = {
-    open: {
-      clipPath: 'circle(1200px at 50px 50px)',
-      transition: {
-        type: 'spring',
-        stiffness: 20,
-      },
-    },
-
-    closed: {
-      clipPath: 'circle(30px at 50px 50px)',
-      transition: {
-        delay: 0.5,
-        type: 'spring',
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     <motion.div className='sidebar' animate={open ? 'open' : 'closed'}>
--- my-portfolio/src/components/sidebar/links/Links.tsx
+++ my-portfolio/src/components/sidebar/links/Links.tsx
@@ -1,4 +1,30 @@
 import React from 'react';
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links: React.FC = () => {
   const items = [
@@ -10,13 +36,14 @@ const Links: React.FC = () => {
   ];
 
   return (
-    <div className='links'>
+    <motion.div className='links' variants={variants}>
       {items.map(item => (
-        <a href={`#${item}`} key={item}>
+        <motion.a href={`#${item}`} key={item}
+         variants={itemVariants} whileHover={{scale: 1.1}} whileTap={{scale: 0.90}}>
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 

e27c07e1a83a58c20b7090b0fdaaddf5b33d66ad
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-02-26 01:09:45 +0100

    .

--- my-portfolio/src/components/Navbar/Navbar.tsx
+++ my-portfolio/src/components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ const navbar: React.FC = () => {
       <div className='wrapper'>
         <motion.span initial={{opacity: 0, scale: 0.5}}
          animate={{opacity: 1, scale: 1}} 
-         transition={{duration: 0.5}}>Tarcísio Menezes</motion.span>
+         transition={{duration: 0.5}}></motion.span>
         <div className='social'>
             <a href='https://github.com/tarcisiomenezes2023' target='blank'><img src='/github.png' alt='github'/></a>
             <a href='https://www.linkedin.com/in/tarc%C3%ADsio-menezes-03b8a0233/' target='blank'><img src='/linkedin.png' alt='linkedln'/></a>

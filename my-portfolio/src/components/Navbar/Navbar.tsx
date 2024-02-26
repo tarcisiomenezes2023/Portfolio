42541cd75c17e6938f5fed2c1ca20f8f1f998237
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-02-26 09:19:48 +0100

    Hero Icons and Hero section implemented

--- my-portfolio/src/components/Navbar/Navbar.tsx
+++ my-portfolio/src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const navbar: React.FC = () => {
         <motion.span initial={{opacity: 0, scale: 0.5}}
          animate={{opacity: 1, scale: 1}} 
          transition={{duration: 0.5}}></motion.span>
-        <div className='social'>
+        <div className='Logo'>
           <img src='/Mylogo2.png' alt='' />
         </div>
       </div>
--- my-portfolio/src/components/hero/Hero.tsx
+++ my-portfolio/src/components/hero/Hero.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import "./hero.scss"
+import { px } from 'framer-motion'
 
 const Hero: React.FC = () => {
   return (
     <div className='hero'>
         <div className='textContainer'>
-        <h2>TARCÍSIO MENEZES</h2>
-        <h1>Web Developer</h1>
+            <h2>TARCÍSIO MENEZES</h2>
+            <h1>Web Developer</h1>
+        <div className='social'>
+            <img src='/github.png' alt='' />
+            <img src='/linkedin.png' alt='' />
+            <img src='/instagram.png' alt='' />
+            <img src='/facebook1.png' alt='' />
+            <img src='outlook.png' alt='' />
+        </div>
+        
+        <svg height="40px" width="40px" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"></path>
+        </svg>
         </div>
       <div className='imageContainer'>
         <img src='/hero.png' alt='My profile' />
